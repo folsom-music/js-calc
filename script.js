cdabe3b06df4c196670e8a24b3dbcfa444281b66
Peter Flom <flom.peter@gmail.com>
2021-04-17 23:04:00 -0600

    still working on refactor, adding new functions for key input

--- script.js
+++ script.js
@@ -1,142 +1,74 @@
-const display = document.querySelector('.screen');
-const funcButtons = document.querySelectorAll('.func-btn');
-const numButtons = document.querySelectorAll('.num-btn');
+const calcButtons = document.querySelectorAll('.func-btn, .num-btn');
 
-window.addEventListener('keydown', (e) => { pressKey(e.key) });
+let calculator = {
+    displayStr: '0',
+    firstVal: null,
+    operator: null,
+    hasInputs: false
+};
 
-numButtons.forEach(button =>
-    button.addEventListener('click', (e) => { pressKey(button.id)})
-);
-funcButtons.forEach(button =>
-    button.addEventListener('click', (e) => { pressKey(button.id)})
-);
-
-function pressKey(key) {
-    if (key === "Backspace") {
-        inputKey(key);
-    } else if (display.textContent.length > 7) {
-        return;
-    } else {
-        inputKey(key);
-    };
+function updateDisplay() {
+    const display = document.querySelector('.screen');
+    display.textContent = calculator.displayStr;
 }
 
-function inputKey(char) {
-    switch (char) {
-        default:
-            break;
-        case '.':
-            inputKey('decimal');
-            break;
-        case '=':
-        case 'Enter':
-            inputKey('equals');
-            break;
-        case 'Escape':
-        case 'c':
-            inputKey('clear');
-            break;
-        case '+':
-            inputKey('plus');
-            break;
-        case '*':
-        case 'X':
-        case 'x':
-            inputKey('multiply');
-            break;
-        case '/':
-            inputKey('divide');
-            break;
-        case '0':
-            preventLeadingZeros();
-            break;
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            removeLeadingZeros();
-            display.textContent += char;
-            break;
-        case 'decimal':
-            displayFloat();
-            break;
-        case 'equals':
-            operate(operator, firstNum, lastNum);
-            break;
-        case 'Backspace':
-            removeLastChar();
-            break;
-        case 'clear':
-            clearCalc();
-            break;
-        case '-':
-            assignHyphen();
-            break;
+window.addEventListener('keydown', (e) => { inputFilter(e.key) });
+
+calcButtons.forEach(button =>
+        button.addEventListener('click', (e) => { inputFilter(button.id)})
+);
+
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a, b) { return a / b; }
+
+function operate(op, a, b) {
+    switch (op) {
         case 'plus':
+            return add(a, b);
         case 'minus':
+            return subtract(a, b);
         case 'multiply':
+            return multiply(a, b);
         case 'divide':
-            assignOperator(char);
-            break;
-        case 'mem-rc':
-            memoryRecall();
-        case 'mem-plus':
-        case 'mem-minus':
-        case 'invert':
-        case 'sq-rt':
-        case 'percent':
-            instantFunc(char);
-            break;
-    };
-}
-
-function preventLeadingZeros(){
-    if (display.textContent === "0") {
-        return;
-    } else if (display.textContent === "") {
-        display.textContent = "0";
-    } else {
-        display.textContent += "0";
-    };
-}
-
-function removeLeadingZeros() {
-    if (display.textContent[0] === "0" && display.textContent.length > 0 && !display.textContent.includes(".")) {
-        display.textContent = display.textContent.substring(1);
-    };
-}
-
-function displayFloat() {
-    if (display.textContent.includes(".")) {
-        return;
-    } else if (display.textContent === "") {
-        display.textContent += "0.";
-    } else {
-        display.textContent += ".";
-    };
-}
-function operate() {}
-function removeLastChar() {
-    display.textContent = display.textContent.slice(0, -1);
-    if (display.textContent.length < 1) {
-        display.textContent = "0";
-    };
-}
-function clearCalc() {}
-
-function assignHyphen() {
-    if (display.textContent.length === 0) {
-        display.textContent += "-";
-    } else {
-        inputKey("minus");
+            return divide(a, b);
     }
 }
 
-function assignOperator() {}
-function memoryRecall() {}
-function instantFunc() {}
+//make an object/array of all avaialble function strings, including the html buttons
+const inputMap = {
+    "=": "equals",
+    "Enter": "equals",
+    ".": "decimal",
+    "Escape": "clear",
+    "c": "clear",
+    "+": "plus",
+    "*": "multiply",
+    "X": "multiply",
+    "x": "multiply",
+    "/": "divide",
+};
+
+function pressNum(str) {
+    const { displayStr } = calculator;
+    calculator.displayStr = displayStr === '0' ? str : displayStr + str;
+    updateDisplay();
+}
+
+function pressDecimal() {
+    if (!calculator.displayStr.includes(".")) {
+        calculator.displayStr += ".";
+    };
+    updateDisplay();
+}
+
+function inputFilter(input) {
+    if (inputMap.hasOwnProperty(input)) {
+        inputFilter(inputMap[input]);
+    } else if (input >= 0  && input <=9) {
+        pressNum(input);
+    } else if (input === "decimal") {
+        pressDecimal();
+    }
+}
